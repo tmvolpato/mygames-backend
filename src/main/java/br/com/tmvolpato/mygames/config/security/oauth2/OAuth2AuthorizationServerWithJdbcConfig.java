60ea197f77281199558276ac6531121e3a4fcc3b
Thiago M. Volpato <tmvolpato@gmail.com>
2019-01-07 23:03:32 -0200

    Ip fixo do docker postgres

--- src/main/java/br/com/tmvolpato/mygames/config/security/oauth2/OAuth2AuthorizationServerWithJdbcConfig.java
+++ src/main/java/br/com/tmvolpato/mygames/config/security/oauth2/OAuth2AuthorizationServerWithJdbcConfig.java
@@ -13,8 +13,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.approval.ApprovalStore;
-import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
